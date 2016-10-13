b911baab0e8407f0342afd22846eac79349f28c4
Agustin Scaramuzza <agustinscaramuzza@gmail.com>
2016-10-13 18:47:11 -0300

    Add models

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class BackScratcher(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    sizes = ArrayField(
+        models.CharField(max_length=2)
+    )
--- src/scratchbling/settings.py
+++ src/scratchbling/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'scratchbling.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'scratchbling',
+        'USER': 'postgres',
+        'PASSWORD': 'pass',
+        'HOST': '172.20.0.15',
+        'PORT': '5432',
     }
 }
 
